4632f008529ce2449231c4a7d092f55a7cfc2360
kthtei <45953099+kthtei@users.noreply.github.com>
2018-12-24 21:57:44 -0500

    Update RecursionMethod.java

--- src/recursionmachine/RecursionMethod.java
+++ src/recursionmachine/RecursionMethod.java
@@ -42,7 +42,7 @@ public class RecursionMethod {
 
     public static String allStar(String str) {
         if (str.equals("")) return "";
-        if (str.length() == 1) return str.substring(0);
+        if (str.length() == 1) return str;
         return str.charAt(0) + "*" + allStar(str.substring(1));
     }
 
@@ -54,8 +54,42 @@ public class RecursionMethod {
 
     public static int strDist(String str, String sub) {
         if (str.equals("")) return 0;
-        if (!str.startsWith(sub)) return strDist(str.substring(1, str.length()), sub);
+        if (!str.startsWith(sub)) return strDist(str.substring(1), sub);
         if (!str.endsWith(sub)) return strDist (str.substring(0, str.length()-1), sub);
         return str.length();
+    }
+    
+    public static boolean groupSum6(int start, int[] nums, int target) {
+        if(start >= nums.length){
+            return target == 0;
+        }
+
+        if(nums[start] != 6 && groupSum6(start + 1, nums, target)){
+            return true;
+        }
+        return groupSum6(start+1, nums, target - nums[start]);
+    }
+
+    public static boolean groupNoAdj(int start, int[] nums, int target) {
+        if (start >= nums.length) return (target == 0);
+
+        if (groupNoAdj(start + 2, nums, target - nums[start])) return true;
+        if (groupNoAdj(start + 1, nums, target)) return true;
+        return false;
+    }
+
+    /**
+     * 
+     * @param str random strings that has a numbers in format of "1, 2, 3, 4" with random length.
+     * @return Return an int array to pass to the methods that take an array as an argument
+     */
+    public static int[] createAnIntArray(String str) {
+        String[] strArray = str.split(",");
+        int[] intArray = new int[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            strArray[i] = strArray[i].trim();
+            intArray[i] = Integer.parseInt(strArray[i]);
+        }
+        return intArray;
     }
 }
